07e6974fe54c447e70fe780139f7f13624fd7a40
iannathan-k <ianmtv@gmail.com>
2026-02-07 22:01:06 -0800

    milestone 1, struct initialized and printed

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@ using namespace std;
 // COMSC-210 | Lab 4A | Ian Kusmiantoro
 
 int main() {
+
+    struct Color {
+        int red;
+        int green;
+        int blue;
+    };
+
+    Color c = {67, 420, 69};
+
+    cout << "red: " << c.red << endl;
+    cout << "green: " << c.green << endl;
+    cout << "blue: " << c.blue << endl;
+
     return 0;
 }
